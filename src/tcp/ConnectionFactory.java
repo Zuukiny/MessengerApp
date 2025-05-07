33e0baa33299ccd456a0f6dd346abfbecffa6d46
Zuukiny <zgc.zuukiny@gmail.com>
2025-05-07 20:04:58 +0200

    modify files so the tests work properly again.

--- src/tcp/ConnectionFactory.java
+++ src/tcp/ConnectionFactory.java
@@ -1,57 +1,29 @@
 package tcp;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.rmi.UnknownHostException;
 
 public class ConnectionFactory {
-    public final int DEFAULT_PORT = 7777;
+    public static final int DEFAULT_PORT = 7777;
     private final int PORT;
-    private final ConnectionHandler connectionHander;
+    private final ConnectionHandler connectionHandler;
 
-
-    ConnectionFactory(ConnectionHandler connectionHandler) {
-        this.PORT = DEFAULT_PORT;
-        this.connectionHander = connectionHandler;
+    public ConnectionFactory(ConnectionHandler connectionHandler) {
+        this(DEFAULT_PORT, connectionHandler);
     }
-
-    ConnectionFactory(int port, ConnectionHandler connectionHandler) {
+    public ConnectionFactory(int port, ConnectionHandler connectionHandler) {
         this.PORT = port;
-        this.connectionHander = connectionHandler;
+        this.connectionHandler = connectionHandler;
     }
 
-
-    /* Opens a ServerSocket for other sockets to connect to */
-    public void acceptNewConnections()
-            throws IOException, UnknownHostException {
-
-            // Create Server socket
-            ServerSocket serverSocket = new ServerSocket(PORT);
-            Socket newConnection = serverSocket.accept();
-
-            System.out.println("Connection established");
-
-            handleConnection(newConnection);
-
-    }
-
-
-    /* Send and receive ByteStreams */
-    private void handleConnection(Socket connectionSocket )
-            throws IOException {
-
-        // write
-        OutputStream connectionSocketOut = connectionSocket.getOutputStream();
-        connectionSocketOut.write(1);
-        System.out.println("Sent to client: 1");
-
-        // read
-        InputStream connectionSocketIn = connectionSocket.getInputStream();
-        int message = connectionSocketIn.read();
-        System.out.println("Received from client: " + message);
+    void acceptNewConnections() throws IOException {
+        ServerSocket server = new ServerSocket(this.PORT);
+        Socket connectionToClient = server.accept();
+        // protocol engine will take over
+        this.connectionHandler.handleConnection(
+                connectionToClient.getInputStream(),
+                connectionToClient.getOutputStream());
 
     }
 }
