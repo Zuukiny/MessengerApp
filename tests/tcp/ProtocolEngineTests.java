322cd4a30355c01fcdc6d5931eac19a119bdf6c3
Zuukiny <zgc.zuukiny@gmail.com>
2025-05-08 16:46:16 +0200

    Implement the tests for the DataStreamServer protocol. The new sections contains three tests (runDataStreamServer), (testDataStreamServerValid) and (testDataStreamServerInvalid). The latter intentionally reads in the wrong order resulting in a loss of information, due to the misalligning between which byte belongs to which data type/variable.

--- tests/tcp/ProtocolEngineTests.java
+++ tests/tcp/ProtocolEngineTests.java
@@ -3,7 +3,10 @@ package tcp;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.EOFException;
 import java.net.Socket;
 
 public class ProtocolEngineTests {
@@ -88,5 +91,119 @@ public class ProtocolEngineTests {
 
         // 4. test if incremented correctly
         Assertions.assertEquals(originalMessage + Math.min(HowManyIncrements, 10), readMessage);
+    }
+
+///////////////////////////////////////////////////////////////////////////////////////
+//                               IncrementServer                                     //
+///////////////////////////////////////////////////////////////////////////////////////
+
+    @Test
+    public void runDataStreamServer() throws IOException {
+        ConnectionFactory connectionFactory = new ConnectionFactory(new DatastreamServer());
+        System.out.println("test: going to accept new connections");
+        connectionFactory.acceptNewConnections();
+    }
+
+    @Test
+    public void testDataStreamServerValid () throws IOException {
+        Socket newConnection = new Socket("localhost", 7777);
+
+        // send message
+        long longSendValue = 190000000L; // 190.000.000
+        int intSendValue = 420000; // 420.000
+        String stringSendValue = "Hello World";
+
+        // receive message
+        long longReadValue;
+        int intReadValue;
+        String stringReadValue;
+
+        // Bind DataIn- & DataOutputStream to the corresponding Stream of the Socket
+        DataOutputStream dos = new DataOutputStream(newConnection.getOutputStream());
+        DataInputStream dis = new DataInputStream(newConnection.getInputStream());
+
+        // Send bytes in the following order
+        //      - Long
+        //      - Int
+        //      - String
+
+        dos.writeLong(longSendValue);
+        dos.writeInt(intSendValue);
+        dos.writeUTF(stringSendValue);
+
+        System.out.println("sent long: " + longSendValue);
+        System.out.println("sent int: " + intSendValue);
+        System.out.println("sent String: " + stringSendValue);
+
+        // Read bytes in the following order
+        //      - String
+        //      - Int
+        //      - Long
+
+        stringReadValue = dis.readUTF();
+        intReadValue = dis.readInt();
+        longReadValue = dis.readLong();
+
+        System.out.println("read String: " + stringReadValue);
+        System.out.println("read int: " + intReadValue);
+        System.out.println("read Long: " + longReadValue);
+
+
+        // Assertion tests
+        Assertions.assertEquals(longSendValue, longReadValue);
+        Assertions.assertEquals(intSendValue, intReadValue);
+        Assertions.assertEquals(stringSendValue, stringReadValue);
+    }
+
+    @Test
+    public void testDataStreamServerInvalid() throws IOException, EOFException {
+        Socket newConnection = new Socket("localhost", 7777);
+
+        // send message
+        long longSendValue = 190000000L;
+        int intSendValue = 420000;
+        String stringSendValue = "Hello World";
+
+        // receive message
+        long longReadValue = 0;
+        int intReadValue = 0;
+        String stringReadValue = "";
+
+        // Bind DataIn- & DataOutputStream to the corresponding Stream of the Socket
+        DataOutputStream dos = new DataOutputStream(newConnection.getOutputStream());
+        DataInputStream dis = new DataInputStream(newConnection.getInputStream());
+
+        // Send bytes in the following order
+        //      - Long
+        //      - Int
+        //      - String
+
+        dos.writeLong(longSendValue);
+        dos.writeInt(intSendValue);
+        dos.writeUTF(stringSendValue);
+
+        System.out.println("sent long: " + longSendValue);
+        System.out.println("sent int: " + intSendValue);
+        System.out.println("sent String: " + stringSendValue);
+
+        ///  ORDER ALTERNATED, DATA IS ALLOCATED INCORRECTLY ///
+        // Read bytes in the following order
+        //      - Int
+        //      - String
+        //      - Long
+
+        intReadValue = dis.readInt();
+        longReadValue = dis.readLong();
+        dos.flush();
+
+        System.out.println("read Int: " + intReadValue);
+        System.out.println("read String: " + stringReadValue);
+        System.out.println("read Long: " + longReadValue);
+
+
+        // Assertion tests
+        Assertions.assertNotEquals(intSendValue, intReadValue);
+        Assertions.assertNotEquals(longSendValue, longReadValue);
+        Assertions.assertNotEquals(stringSendValue, stringReadValue);
     }
 }
