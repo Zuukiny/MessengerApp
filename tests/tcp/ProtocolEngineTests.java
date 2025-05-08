9e08b9c430d3cbea195b3e7806f9ca466ace39f5
Zuukiny <zgc.zuukiny@gmail.com>
2025-05-08 14:53:58 +0200

    Implement the tests for the IncrementServer protocol. The new sections contains two tests (runIncrementServer) and (testIncrementServer). A message is sent and its increment is received. That new increment value is send again and is subsequently incremented again. So on and so forth, up to a maximum of 10 increments to this connection.

--- tests/tcp/ProtocolEngineTests.java
+++ tests/tcp/ProtocolEngineTests.java
@@ -38,4 +38,55 @@ public class ProtocolEngineTests {
         // must be the same - it is an echo server.
         Assertions.assertEquals(byte2Sent, readByte);
     }
+
+///////////////////////////////////////////////////////////////////////////////////////
+//                               IncrementServer                                     //
+///////////////////////////////////////////////////////////////////////////////////////
+
+    @Test
+    public void runIncrementServer() throws IOException {
+        ConnectionFactory connectionFactory = new ConnectionFactory(new IncrementServer());
+        System.out.println("test: going to accept new connections");
+        connectionFactory.acceptNewConnections();
+    }
+
+    @Test
+    public void testIncrementServer() throws IOException {
+        // 1. connect
+        Socket newConnection = new Socket("localhost", 7777);
+        byte HowManyIncrements = 20;
+        byte originalMessage = 42;
+        byte sendMessage;
+        byte readMessage = 0;
+
+        // Differentiation between sendMessage and originalMessage is just for Assertion Test and clear separation of values;
+        sendMessage = originalMessage;
+
+
+        // First sent byte defines, how often this message should be incremented
+        // Maximum increments is capped at 10 per connection.
+        newConnection.getOutputStream().write(HowManyIncrements);
+
+
+        for (int repetition = 0; repetition < HowManyIncrements && repetition < 10; repetition++) {
+            // 2. write message
+            newConnection.getOutputStream().write(sendMessage);
+            System.out.println("sent: " + sendMessage);
+
+            // 3. read message
+            readMessage = (byte) newConnection.getInputStream().read();
+            System.out.println("read: " + readMessage);
+
+            // Set sendMessage to readMessage
+            sendMessage = readMessage;
+
+            // Visually seperate iterations from each other
+            if (repetition < 9) {
+                System.out.println("<<<<< Next iteration: " + (repetition + 1) + " >>>>> ");
+            }
+        }
+
+        // 4. test if incremented correctly
+        Assertions.assertEquals(originalMessage + Math.min(HowManyIncrements, 10), readMessage);
+    }
 }
