5fc17973888a5ecb604743e13c9286c260eae177
Zuukiny <zgc.zuukiny@gmail.com>
2025-06-11 21:14:25 +0200

    Add new file serialization. Not completely finished yet. There still is the socket connection missing.

--- src/utils/MySerialization.java
+++ src/utils/MySerialization.java
@@ -1,9 +1,6 @@
 package utils;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,13 +13,13 @@ public class MySerialization {
         int amountOfInts = intArray.length;
 
         ///  Using Little Endian
-        // Provide information about how much integers will be in the InputStream when written to it.
+        // Provide information about how many integers will be in the OutputStream when written to it.
         byteList.add( (byte) amountOfInts);
         byteList.add( (byte) (amountOfInts >> 8) );
         byteList.add( (byte) (amountOfInts >> 16) );
         byteList.add( (byte) (amountOfInts >> 24) );
 
-        // Convert each Integer to bytes, which later will be written to the InputStream.
+        // Convert each Integer to bytes, which later will be written to the OutputStream.
         for (int integer : intArray) {
             byteList.add( (byte) integer );
             byteList.add( (byte) (integer >> 8) );
@@ -69,5 +66,39 @@ public class MySerialization {
         is.close();
 
         return deserializedArray;
+    }
+
+    public void serializeFile(File sourceFile, OutputStream os) throws IOException {
+        // File muss deserialized werden und in den os (welcher ein daos ist) geschickt werden.
+
+        long lenghtOfFile = sourceFile.length();
+        FileInputStream fis = new FileInputStream(sourceFile);
+        List<Byte> byteList = new ArrayList<Byte>();
+
+        ///  Prepare Bytes for sending - Using LITTLE ENDIAN
+        // Provide information about how many bytes will be in the FileOutputStream when written to it.
+        byteList.add( (byte) lenghtOfFile);
+        byteList.add( (byte) (lenghtOfFile >> 8) );
+        byteList.add( (byte) (lenghtOfFile >> 16) );
+        byteList.add( (byte) (lenghtOfFile >> 24) );
+        byteList.add( (byte) (lenghtOfFile >> 32) );
+        byteList.add( (byte) (lenghtOfFile >> 40) );
+        byteList.add( (byte) (lenghtOfFile >> 48) );
+        byteList.add( (byte) (lenghtOfFile >> 56) );
+
+
+        // Read every Byte from the files initial message and write it to the FileInputStream.
+        for (int i = 0; i < lenghtOfFile; i++) {
+            byteList.add((byte) fis.read());
+        }
+
+        ///  Write to FileInputStream
+        for (byte byteValue : byteList) {
+            os.write(byteValue);
+        }
+    }
+
+    public void deserializeFile() {
+        ;
     }
 }
--- tests/utils/SerializationTests.java
+++ tests/utils/SerializationTests.java
@@ -3,10 +3,8 @@ package utils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.net.Socket;
 
 public class SerializationTests {
 
@@ -22,6 +20,35 @@ public class SerializationTests {
         InputStream is = new ByteArrayInputStream(serializedData);
         int[] result = ms.deserialize(is);
         Assertions.assertArrayEquals(sample, result);
+    }
+
+    @Test
+    public void fileTest1() throws IOException {
+        String sampleData = "Hallo Welt, hier ist ein Java Programm. Ich wurde von der Zeile aus in eine Datei geschrieben. Das ist schon zeimlich Banonkas :)";
+        String fileNameSource = "sourceFile.txt";
+        String fileNameTarget = "targetFile.txt";
+
+        // Create temporary file and fill it with some sampleData
+        DataOutputStream daos = new DataOutputStream(new FileOutputStream(fileNameSource));
+        daos.writeUTF(sampleData);
+        daos.close();
+
+
+        // Create two permanent files
+        File sourceFile = new File(fileNameSource);
+        File targetFile = new File(fileNameTarget);
+        MySerialization ms = new MySerialization();
+
+        Socket clientSocket = new Socket("localhost", 7777);
+
+
+        // serialize data of one file into DataOutputStream
+        OutputStream os = clientSocket.getOutputStream();
+        InputStream is = clientSocket.getInputStream();
+
+        ms.serializeFile(sourceFile, os);
+        ms.deserialize(is);
+
     }
 
 }
