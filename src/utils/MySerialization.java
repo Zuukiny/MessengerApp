eae16eb21c0adce9ffdaf986d86c113dcb671f34
Zuukiny <zgc.zuukiny@gmail.com>
2025-06-11 19:43:15 +0200

    Change serialize() so that it also transmits information about how long this stream is going to be. Finish deserialize().

--- src/utils/MySerialization.java
+++ src/utils/MySerialization.java
@@ -1,5 +1,6 @@
 package utils;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -12,8 +13,16 @@ public class MySerialization {
     public void serialize(int[] intArray, OutputStream os) throws IOException {
         // int: FF FF FF 03
         List<Byte> byteList = new ArrayList<Byte>();
+        int amountOfInts = intArray.length;
 
         ///  Using Little Endian
+        // Provide information about how much integers will be in the InputStream when written to it.
+        byteList.add( (byte) amountOfInts);
+        byteList.add( (byte) (amountOfInts >> 8) );
+        byteList.add( (byte) (amountOfInts >> 16) );
+        byteList.add( (byte) (amountOfInts >> 24) );
+
+        // Convert each Integer to bytes, which later will be written to the InputStream.
         for (int integer : intArray) {
             byteList.add( (byte) integer );
             byteList.add( (byte) (integer >> 8) );
@@ -31,10 +40,34 @@ public class MySerialization {
         }
 
         os.write(byteArray);
+        os.close();
     }
 
-    public int[] deserialize(InputStream is) {
-        ;
-        return new int[0];
+    public int[] deserialize(InputStream is) throws IOException {
+
+        // Contains information about how long the InputStream will last (easier measurement for later iteration)
+        byte b1 = (byte) is.read(); // 4
+        byte b2 = (byte) is.read(); // 0
+        byte b3 = (byte) is.read(); // 0
+        byte b4 = (byte) is.read(); // 0
+
+        ///  Received Bytes are formatted in Little Endian
+        // The first read Byte should be the least significant Byte
+        int lengthOfStream = (0xFF & b4) << 24 | ((0xFF & b3) << 16) | ((0xFF & b2) << 8) | (0xFF & b1);
+
+        int[] deserializedArray = new int[lengthOfStream];
+
+        for (int i = 0; i < lengthOfStream; i++) {
+            b1 = (byte) is.read();
+            b2 = (byte) is.read();
+            b3 = (byte) is.read();
+            b4 = (byte) is.read();
+
+            int mergedInteger = (0xFF & b4) << 24 | (0xFF & b3) << 16 | ((0xFF & b2) << 8) | (0xFF & b1);
+            deserializedArray[i] = mergedInteger;
+        }
+        is.close();
+
+        return deserializedArray;
     }
 }
