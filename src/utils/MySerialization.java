8b73b16958d79fdf4b0037385bc5be11c3e34968
Zuukiny <zgc.zuukiny@gmail.com>
2025-06-16 22:31:02 +0200

    Finish deserializeFile() implementation

--- src/utils/MySerialization.java
+++ src/utils/MySerialization.java
@@ -71,24 +71,24 @@ public class MySerialization {
     public void serializeFile(File sourceFile, OutputStream os) throws IOException {
         // File muss deserialized werden und in den os (welcher ein daos ist) geschickt werden.
 
-        long lenghtOfFile = sourceFile.length();
+        long lengthOfFile = sourceFile.length();
         FileInputStream fis = new FileInputStream(sourceFile);
         List<Byte> byteList = new ArrayList<Byte>();
 
         ///  Prepare Bytes for sending - Using LITTLE ENDIAN
-        // Provide information about how many bytes will be in the FileOutputStream when written to it.
-        byteList.add( (byte) lenghtOfFile);
-        byteList.add( (byte) (lenghtOfFile >> 8) );
-        byteList.add( (byte) (lenghtOfFile >> 16) );
-        byteList.add( (byte) (lenghtOfFile >> 24) );
-        byteList.add( (byte) (lenghtOfFile >> 32) );
-        byteList.add( (byte) (lenghtOfFile >> 40) );
-        byteList.add( (byte) (lenghtOfFile >> 48) );
-        byteList.add( (byte) (lenghtOfFile >> 56) );
+        // Provide information about how many bytes will be in the OutputStream when written to it.
+        byteList.add( (byte) lengthOfFile);
+        byteList.add( (byte) (lengthOfFile >> 8) );
+        byteList.add( (byte) (lengthOfFile >> 16) );
+        byteList.add( (byte) (lengthOfFile >> 24) );
+        byteList.add( (byte) (lengthOfFile >> 32) );
+        byteList.add( (byte) (lengthOfFile >> 40) );
+        byteList.add( (byte) (lengthOfFile >> 48) );
+        byteList.add( (byte) (lengthOfFile >> 56) );
 
 
         // Read every Byte from the files initial message and write it to the FileInputStream.
-        for (int i = 0; i < lenghtOfFile; i++) {
+        for (int i = 0; i < lengthOfFile; i++) {
             byteList.add((byte) fis.read());
         }
 
@@ -98,7 +98,33 @@ public class MySerialization {
         }
     }
 
-    public void deserializeFile() {
-        ;
+    public void deserializeFile(File targetFile, InputStream is) throws IOException {
+        List<Byte> byteList = new ArrayList<Byte>();
+        ///  Prepare read cycle
+        // read first 8 Bytes which contain the information about file length
+        long fileLength;
+
+        byte b1 = (byte) is.read();
+        byte b2 = (byte) is.read();
+        byte b3 = (byte) is.read();
+        byte b4 = (byte) is.read();
+        byte b5 = (byte) is.read();
+        byte b6 = (byte) is.read();
+        byte b7 = (byte) is.read();
+        byte b8 = (byte) is.read();
+
+        // Convert the read bytes and merge them to a long representing the fileSize (excl. the info-Bytes about the length of file)
+        fileLength =  (long) (b8) << 56 | (long) (0xFF & b7) << 48 | (long) (0xFF & b6) << 40 | (long) (0xFF & b5) << 32 |
+                (long) (0xFF & b4) << 24 | (long) (0xFF & b3) << 16 | (long) (0xFF & b2) << 8 | (long) (0xFF & b1);
+
+        // Create FileOutputStream that connects to the file
+        FileOutputStream fos = new FileOutputStream(targetFile);
+
+        int writeToFileOutputStreamUntilEOF = 0;
+        while (writeToFileOutputStreamUntilEOF < fileLength) {
+            byte readByteFromInputStream = (byte) is.read();
+            fos.write(readByteFromInputStream);
+            writeToFileOutputStreamUntilEOF++;
+        }
     }
 }
