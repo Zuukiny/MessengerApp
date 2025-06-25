021888e25e964cca84c01397b2fd2ad3729389c8
Zuukiny <zgc.zuukiny@gmail.com>
2025-06-25 21:56:19 +0200

    Add Code for sending ERROR-PDU, OK-PDU and GET-PDU. Some error Code have been added that are sent with the ERROR PDU. Make fields public as they are class variables. Remove getter as this class will have an instance.

--- src/simpleFileServer/PDU.java
+++ src/simpleFileServer/PDU.java
@@ -3,14 +3,24 @@ package simpleFileServer;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
-public class PDU {
+public abstract class PDU {
     public static final byte PROTOCOL_VERSION = 1;
 
     // Command Codes
-    private static final byte GET_PDU = 0x00;
-    private static final byte PUT_PDU = 0x01;
-    private static final byte ERROR = 0x02;
-    private static final byte OK = 0x03;
+    public static final byte GET_PDU = 0x00;
+    public static final byte PUT_PDU = 0x01;
+    public static final byte ERROR = 0x02;
+    public static final byte OK = 0x03;
+
+    // Error Codes
+    public static final int FILE_NOT_FOUND = 0x64; // 100
+    public static final int INVALID_COMMAND = 0x65; // 101
+
+    public static void writeGETPDU(DataOutputStream daos, String fileName) throws IOException {
+        daos.writeByte(PROTOCOL_VERSION);
+        daos.writeByte(GET_PDU);
+        daos.writeUTF(fileName);
+    }
 
     public static void writePUTPDU(DataOutputStream daos, String fileName) throws IOException {
         daos.writeByte(PROTOCOL_VERSION);
@@ -18,20 +28,17 @@ public class PDU {
         daos.writeUTF(fileName);
     }
 
-
-    public byte getGet_Pdu() {
-        return GET_PDU;
+    public static void writeERROR(DataOutputStream daos, String fileName, int errorCode, String errorMessage) throws IOException {
+        daos.writeByte(PROTOCOL_VERSION);
+        daos.writeByte(ERROR);
+        daos.writeUTF(fileName);
+        daos.write(errorCode);
+        daos.writeUTF(errorMessage);
     }
 
-    public byte getPut_PDU() {
-        return PUT_PDU;
-    }
-
-    public byte getError() {
-        return ERROR;
-    }
-
-    public byte getOK() {
-        return OK;
+    public static void writeOK(DataOutputStream daos, String fileName) throws IOException {
+        daos.writeByte(PROTOCOL_VERSION);
+        daos.writeByte(OK);
+        daos.writeUTF(fileName);
     }
 }
