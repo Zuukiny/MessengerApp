1dd4ea483b94bb8af6d659a382de93275bb40d55
Zuukiny <zgc.zuukiny@gmail.com>
2025-06-25 20:45:39 +0200

    Add the client that can put or get a File from another source (another end of socket connection).

--- src/simpleFileServer/ClientProtocolMachine.java
+++ src/simpleFileServer/ClientProtocolMachine.java
@@ -1,4 +1,28 @@
 package simpleFileServer;
 
+import java.io.*;
+import java.net.Socket;
+
 public class ClientProtocolMachine {
+    Socket clientSocket;
+
+    public void getFile(String fileName) {
+
+    }
+
+    public void putFile(String fileName) throws FileNotFoundException, IOException {
+        File fileToSend = new File("/resources/" + fileName + ".txt");
+
+        DataOutputStream dos = new DataOutputStream(clientSocket.getOutputStream());
+        DataInputStream dis = new DataInputStream(new FileInputStream(fileToSend));
+
+        byte[] buffer = new byte[1024];
+        int messageBytes;
+
+        PDU.writePUTPDU(dos, fileName); // Write PDU package metadata to the stream beforehand
+        dos.writeLong(fileToSend.length());
+        while ((messageBytes = dis.read(buffer)) != -1) {
+            dos.write(buffer, 0, messageBytes);
+        }
+    }
 }
