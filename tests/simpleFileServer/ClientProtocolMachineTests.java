ba747f65bbb53862c0a30561f83731217d4e85ad
Zuukiny <zgc.zuukiny@gmail.com>
2025-06-25 21:57:54 +0200

    Add putFileTest() - NOT FULLY IMPLEMENTED.

--- tests/simpleFileServer/ClientProtocolMachineTests.java
+++ tests/simpleFileServer/ClientProtocolMachineTests.java
@@ -1,4 +1,22 @@
 package simpleFileServer;
 
-public class clientProtocolMachineTests {
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class ClientProtocolMachineTests {
+
+    @Test
+    public void putFileTest() {
+        String fileToSend = "resources/UmbreonPokedexEntry.txt";
+
+        ClientProtocolMachine client = new ClientProtocolMachine();
+        try {
+            client.putFile(fileToSend);
+        }
+        catch(IOException ioe) {
+            System.out.println("Stream behaved weirdly: " + ioe.getMessage());
+        }
+    }
 }
