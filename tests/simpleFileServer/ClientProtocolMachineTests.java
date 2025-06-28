62db18218364e910b674589afb6580dc65176834
Zuukiny <zgc.zuukiny@gmail.com>
2025-06-28 15:55:34 +0200

    Finish tests -> They are always sort of true, you just have to manualy check, whether or not a copy of the File was created thta has the same data

--- tests/simpleFileServer/ClientProtocolMachineTests.java
+++ tests/simpleFileServer/ClientProtocolMachineTests.java
@@ -2,21 +2,32 @@ package simpleFileServer;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.Socket;
 
 public class ClientProtocolMachineTests {
 
     @Test
-    public void putFileTest() {
-        String fileToSend = "resources/UmbreonPokedexEntry.txt";
+    public void sendFileToTesterPeer() throws IOException {
+        Socket clientSocket = new Socket("localhost", 4444);
 
-        ClientProtocolMachine client = new ClientProtocolMachine();
-        try {
-            client.putFile(fileToSend);
-        }
-        catch(IOException ioe) {
-            System.out.println("Stream behaved weirdly: " + ioe.getMessage());
-        }
+        ClientProtocolMachine client = new ClientProtocolMachine(clientSocket.getInputStream(), clientSocket.getOutputStream());
+
+        String fileToSend = "resources/test.txt";
+        client.putFile(fileToSend);
+
+        ///  Test: See BNTesterPeer
+    }
+
+    @Test
+    public void getFileFromTesterPeer() throws IOException {
+        Socket clientSocket = new Socket("localhost", 7777);
+
+        ClientProtocolMachine client = new ClientProtocolMachine(clientSocket.getInputStream(), clientSocket.getOutputStream());
+
+        String fileToReceive = "sourceFile.txt";
+        client.getFile(fileToReceive);
+
+        ///  Test: See BNTesterPeer
     }
 }
--- tests/simpleFileServer/ClientServerTests.java
+++ tests/simpleFileServer/ClientServerTests.java
@@ -1,4 +1,22 @@
 package simpleFileServer;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ClientServerTests {
+
+    @Test
+    public void testServer() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(7777);
+
+        Socket clientSocket = serverSocket.accept();
+
+        ClientServer server = new ClientServer(clientSocket);
+        server.handle();
+
+    }
 }
